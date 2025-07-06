daee5e977cbe5bcd7e9f4c51759f09ee04aaa1c8
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-07-06 19:39:21 +0530

    12.Cube_Root_Of_A_Number.cpp

--- 12.Cube_Root_Of_A_Number.cpp
+++ 12.Cube_Root_Of_A_Number.cpp
@@ -10,3 +10,15 @@ class Solution {
 };
 
 //we can do it by using both pow and cbrt function. And sometime we can do it by writing double also
+---------------------------------------------------------------------------------------------------
+  //Java Code
+  // User function Template for Java
+
+class Solution {
+    static int cubeRoot(int n) {
+        // code here
+        float ans =(float)Math.pow(n,(1.0/3));
+        int floorVal = (int)Math.floor(ans);
+        return floorVal;
+    }
+};
