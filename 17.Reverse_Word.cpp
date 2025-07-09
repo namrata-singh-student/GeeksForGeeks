65aa72decef8bf45bee3eacf753a6943fa1daf50
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-07-09 20:00:58 +0530

    17.Reverse_Word.cpp

---
+++ 17.Reverse_Word.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  public:
+    string reverseWords(string &s) {
+        // code here
+        int n =s.length();
+        reverse(s.begin(),s.end());
+        int i =0;
+        int r=0, l=0;
+        while(i<n){
+            while(i<n && s[i]!=' '){
+                s[r] = s[i];
+                r++;
+                i++;
+            }
+            if(l<r){
+                reverse(s.begin()+l , s.begin()+r);
+                s[r]=' ';
+                r++;
+                l = r;
+            }
+            i++;
+        }
+        s= s.substr(0,r-1);
+        return s;
+        // stringstream ss(s);
+        // string token ;
+        // string result ="";
+        // while(ss >> token){
+        //     result = token+" "+result;
+        // }
+        // return result.substr(0,n-1);
+    }
+};
